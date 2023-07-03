91d882d7d7a1b1d90d224dce502dc6a23c6e58b9
Debojit Kumar Das <debojit2997@gmail.com>
2023-07-03 22:13:40 +0530

    solving problems

---
+++ Kernighan_and_Ritchie/chapter_01/exercise_1-11.c
@@ -0,0 +1,37 @@
+/* Exercise 1-11. How would you test the word count program? What kinds of input are most likely to uncover bugs if there are any? */
+
+/* the word count program */
+#include <stdio.h>
+#include <string.h>
+
+#define IN  1
+#define OUT 0
+
+int main() {
+
+    char test_string[] = "Hello, World!! This is a word counting program.\n";
+    long str_len = strlen(test_string);
+
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    for (int i = 0; i < str_len; ++i) {
+        c = test_string[i];
+        ++nc;
+        if (c == '\n')
+            ++nl;
+        if (c == ' ' || c == '\t' || c == '\n')
+            state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
+
+    printf("Number of lines: %d\n", nl);
+    printf("Number of words: %d\n", nw);
+    printf("Number of characters: %d\n", nc);
+
+    return 0;
+}
