86f9878b75b3ffdc0e8398af5b85b6b865912ce3
Debojit Kumar Das <debojit2997@gmail.com>
2023-07-05 10:48:30 +0530

    solving problems

--- Kernighan_and_Ritchie/chapter_01/exercise_1-11.c
+++ Kernighan_and_Ritchie/chapter_01/exercise_1-11.c
@@ -8,16 +8,15 @@
 #define IN  1
 #define OUT 0
 
-int main() {
+void run_test(char *test_string) {
 
-    char test_string[] = "Hello, World!! This is a word counting program.\n";
-
-    int c, nl, nw, nc, state;
+    int nl, nw, nc, state;
+    char c;
 
     state = OUT;
     nl = nw = nc = 0;
-    for (int i = 0; test_string[i] != 0; ++i) {
-        c = test_string[i];
+    while (*test_string != '\0') {
+        c = *test_string;
         ++nc;
         if (c == '\n')
             ++nl;
@@ -27,11 +26,40 @@ int main() {
             state = IN;
             ++nw;
         }
+        test_string++;
     }
 
     printf("Number of lines: %d\n", nl);
     printf("Number of words: %d\n", nw);
     printf("Number of characters: %d\n", nc);
+}
+
+int main() {
+
+    char test_string_01[] = "Hello, World!! This is a word counting program.\n";
+    char test_string_02[] = " \t \n \n \n";
+    char test_string_03[] = ".... \t ... \n";
+    char test_string_04[] = "String are fun. I wonder the things I'll disc-\nover while working through this exercises.\n";
+
+    printf("Running test 01 for test string:\n");
+    printf("%s\n", test_string_01);
+    run_test(test_string_01);
+    printf("\n\n");
+
+    printf("Running test 02 for test string:\n");
+    printf("%s\n", test_string_02);
+    run_test(test_string_02);
+    printf("\n\n");
+
+    printf("Running test 03 for test string:\n");
+    printf("%s\n", test_string_03);
+    run_test(test_string_03);
+    printf("\n\n");
+
+    printf("Running test 04 for test string:\n");
+    printf("%s\n", test_string_04);
+    run_test(test_string_04);
+    printf("\n\n");
 
     return 0;
 }
