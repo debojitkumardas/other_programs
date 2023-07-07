eddd553576499d7e2f544ec58e673ac81012110b
Debojit Kumar Das <debojit2997@gmail.com>
2023-07-08 05:16:21 +0530

    Update

--- Jane_street_puzzles/2023_07_Puzzle/main.cpp
+++ Jane_street_puzzles/2023_07_Puzzle/main.cpp
@@ -6,10 +6,24 @@
 *   * all number clues in the grid give the size of the region (shaded and unshaded) that the clue is in.
 * The answer to this puzzle is the product of the number os unshaded cells in each row.
 *   ******************************************** */
-
+// {' ', '6', ' ', ' ', ' ', ' ', ' '}
 #include <iostream>
 
+void grid() {
+    char grid_data[7][7] = {
+        {' ', '6', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', '3'},
+        {' ', ' ', ' ', '3', ' ', ' ', ' '},
+        {' ', ' ', '6', '1', '5', ' ', ' '},
+        {' ', ' ', ' ', '4', ' ', ' ', ' '},
+        {'5', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', '4', ' '},
+    };
+}
+
 int main(int argc, char *argv[]) {
+
+    //
 
     return 0;
 }
