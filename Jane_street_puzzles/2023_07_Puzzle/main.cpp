4c2c3ce3fe19f6e84aec2fbac13ec3297f937318
Debojit Kumar Das <debojit2997@gmail.com>
2023-07-07 21:55:07 +0530

    Working on this months (July-23) Jane street puzzle

---
+++ Jane_street_puzzles/2023_07_Puzzle/main.cpp
@@ -0,0 +1,15 @@
+/* *********************************************
+* Given a m x n ...
+* If we shade a subset of the cells in the above grid, we partition the cells into orthogonally connected regions of shaded and unshaded cells. The goal of this puzzle is to shade the cells so that
+*   * all regions of shaded cells are rectangular,
+*   * all regions of unshaded cells are not rectangular, and
+*   * all number clues in the grid give the size of the region (shaded and unshaded) that the clue is in.
+* The answer to this puzzle is the product of the number os unshaded cells in each row.
+*   ******************************************** */
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
