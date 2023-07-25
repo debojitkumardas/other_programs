7f66f432655a5cfd3601c8c169a9a90bc4b8ca39
Debojit Kumar Das <debojit2997@gmail.com>
2023-07-25 17:41:43 +0530

    solving leetcode problems

---
+++ leetcode_problems/234_palindrome_linked_list/helper.c
@@ -0,0 +1,29 @@
+#include "helper.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void insert_node(node **head, int value) {
+    node *new_node = malloc(sizeof(node));
+
+    if (new_node != NULL) {
+        new_node->value = value;
+        new_node->next = *head;
+        *head = new_node;
+    }
+}
+
+void print_list(node *head) {
+    while (head != NULL) {
+        printf("%d, ", head->value);
+        head = head->next;
+    }
+    printf("\\0\n");
+}
+
+void delete_list(node **head) {
+    while (*head != NULL) {
+        node *temp = *head;
+        *head = (*head)->next;
+        free(temp);
+    }
+}
---
+++ leetcode_problems/234_palindrome_linked_list/main.c
@@ -0,0 +1,114 @@
+#include "helper.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define LIST_SZ 10
+
+typedef enum {FALSE, TRUE} bool_;
+
+
+bool_ is_palindrome(node *head) {
+    int len = 0;
+    node *temp = head;
+    bool_ res = TRUE;
+
+    while (temp != NULL) {
+        len++;
+        temp = temp->next;
+    }
+
+    int *ptr_1 = malloc(sizeof(int) * len);
+    int *ptr_2 = malloc(sizeof(int) * len);
+    int i = 0, j = len - 1;
+
+    temp = head;
+    while (temp != NULL) {
+        ptr_1[i] = temp->value;
+        ptr_2[j] = temp->value;
+        i++;
+        j--;
+        temp = temp->next;
+    }
+
+    for (int k = 0; k < len; ++k)
+        if (ptr_1[k] != ptr_2[k])
+            res = FALSE;
+
+    free(ptr_1);
+    free(ptr_2);
+
+    return res;
+}
+
+// solution in time ~ O(n) and space ~ O(1)
+bool_ is_palindrome_adv(node *head) {
+
+    int len = 0, mid = 1;
+    node *ptr = NULL, *temp = NULL;
+    bool_ res = TRUE;
+
+    temp = head;
+    ptr = head;
+    while (temp != NULL) {
+        len++;
+        if (len >= 2 * mid) {
+            ptr = ptr->next;
+            mid++;
+        }
+        temp = temp->next;
+    }
+
+    if (len % 2 != 0)
+        ptr = ptr->next;
+
+    // reversing the linked list
+    node *n_head = NULL;
+    temp = NULL;
+    while (ptr != NULL) {
+        temp = ptr;
+        ptr = ptr->next;
+        temp->next = n_head;
+        n_head = temp;
+    }
+
+    while (n_head != NULL) {
+        if (n_head->value != head->value)
+            res = FALSE;
+
+        n_head = n_head->next;
+        head = head->next;
+    }
+
+    return res;
+}
+
+int main(void) {
+
+    node *head = NULL;
+    char *text[2] = {"FALSE", "TRUE"};
+    bool_ res;
+
+    srand(time(NULL));
+
+    #if NR
+    for (int i = 0; i < LIST_SZ; ++i)
+        insert_node(&head, rand() % 10);
+    #endif
+
+    insert_node(&head, 1);
+    insert_node(&head, 2);
+    insert_node(&head, 3);
+    insert_node(&head, 2);
+    insert_node(&head, 1);
+
+    printf("The list is: ");
+    print_list(head);
+
+    res = is_palindrome_adv(head);
+    printf("Is the given list a palindrome? %s\n", text[res]);
+
+    delete_list(&head);
+
+    return 0;
+}
